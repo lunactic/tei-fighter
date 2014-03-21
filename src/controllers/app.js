9b3f0c794d717ebfb9769d12b5ec33ff47ff1d9a
Joan Pastor <joapaspe@gmail.com>
2014-03-21 09:29:30 +0100

    Preparing stuff

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -5,7 +5,7 @@ teifighterApp.config(['$routeProvider',
     $routeProvider.
       when('/', {
         templateUrl: 'views/main.html',
-        controller: 'DummyController'
+        controller: 'settingsController'
       }).
       when('/info', {
       	templateUrl:'views/info.html',
@@ -15,6 +15,10 @@ teifighterApp.config(['$routeProvider',
       });
   }]);
 
-teifighterApp.controller('DummyController', function(){
+teifighterApp.controller("TeifighterController", teifighterController);
 
-});
+teifighterApp.controller("settingsController", function($scope)
+{
+  $scope.testUrl = "http://digi.ub.uni-heidelberg.de/diglitData/image/cpg148/1/007v.jpg";
+}
+);
---
+++ src/controllers/teifighterController.js
@@ -0,0 +1,6 @@
+teifighterController = function ($scope) {
+	$scope.hello = 'It works';
+	$scope.more = "yes it is";
+}
+
+ 
