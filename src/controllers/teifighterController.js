8d788f4bbc73acc7033a2cc6612e17e2e37a54d0
Mathias <mathias.seuret@unifr.ch>
2014-03-25 15:02:56 +0100

    The image is now displayed. However, there's a glitch when the page is opened in a new tab.

--- src/controllers/teifighterController.js
+++ src/controllers/teifighterController.js
@@ -1,6 +1,23 @@
 teifighterController = function ($scope) {
 	$scope.hello = 'It works';
 	$scope.more = "yes it is";
+	
+	$scope.view
+	
+	$scope.initializeCanvas = function($scope) {
+		// Getting the canva, setting paper
+		var canvas = document.getElementById('canvas');
+		paper.setup(canvas);
+		paper.view.draw();
+		
+		// loading the image
+		var raster = new paper.Raster('image');
+		
+		// Setting correct position
+		raster.position = [raster.width/2, raster.height/2];
+		console.log(raster.position.x+" "+raster.position.y);
+		
+	}
 }
 
  
