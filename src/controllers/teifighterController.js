0711088fed753c8aa4836dd3aefdb5cf9fdc15c0
Mathias <mathias.seuret@unifr.ch>
2014-03-27 16:45:11 +0100

    Mousewheel zoom works on both Firefox and Chromium.

--- src/controllers/teifighterController.js
+++ src/controllers/teifighterController.js
@@ -31,7 +31,6 @@ teifighterController = function ($scope) {
 		var mouseUpPos   = new paper.Point(0,0);
 		var prevDragPos  = new paper.Point(0,0);
 		
-		
 		// Mouseup, there has been a click
 		canvas.onmouseup = function(event) {
 			mouseUpPos.x = event.clientX - canvas.getBoundingClientRect().left;
@@ -41,7 +40,12 @@ teifighterController = function ($scope) {
 			if (mouseUpPos.x==mouseDownPos.x && mouseUpPos.y==mouseDownPos.y) {
 				inputManager.click(mouseUpPos.x, mouseUpPos.y);
 				var q = view.getRealPoint(mouseDownPos);
-				alert(q.x+' '+q.y);
+				var p = view.getViewPoint(q);
+				console.log('Clicked on '+mouseDownPos.x+','+mouseDownPos.y);
+				console.log('Real point '+q.x+','+q.y);
+				console.log('View point '+p.x+','+p.y);
+				
+				setCenter(q);
 			} else {
 				inputManager.dragStopped();
 			}
@@ -68,7 +72,6 @@ teifighterController = function ($scope) {
 			x = event.clientX - canvas.getBoundingClientRect().left;
 			y = event.clientY - canvas.getBoundingClientRect().top;
 			pos = new paper.Point(x,y);
-			console.log(pos);
 			
 			// Should dx,dy be divided by the zoom here, or later ?
 			dx = x - prevDragPos.x;
@@ -80,6 +83,36 @@ teifighterController = function ($scope) {
 			prevDragPos.y = y;
 		}
 		
+		// Zoom-scroll - due to Firefox & Chrome incompatibilities,
+		// it has to be coded twice. Crap.
+		canvas.addEventListener("mousewheel", function (e) {
+			e.preventDefault();
+			var direction = e.deltaY;
+			var viewP = new paper.Point(e.offsetX, e.offsetY);
+			var realP   = getRealPoint(viewP);
+			
+			if (direction<0) {
+				view.zoomIn();
+			} else {
+				view.zoomOut();
+			}
+			view.placePointAt(realP, viewP);
+		});
+		canvas.addEventListener("DOMMouseScroll", function (e) {
+			e.preventDefault();
+			var direction = e.detail;
+			var viewP = new paper.Point(e.layerX, e.layerY);
+			var realP   = getRealPoint(viewP);
+			
+			if (direction<0) {
+				view.zoomIn();
+			} else {
+				view.zoomOut();
+			}
+			view.placePointAt(realP, viewP);
+			console.log(e);
+		}) ;
+		
 
 	};
 	
@@ -88,11 +121,15 @@ teifighterController = function ($scope) {
 	};
 	
 	$scope.buttonZoomIn = function($scope) {
-		view.zoomIn(0,0);
+		var p = view.getCenter();
+		view.zoomIn();
+		view.setCenter(p);
 	};
 	
 	$scope.buttonZoomOut = function($scope) {
+		var p = view.getCenter();
 		view.zoomOut();
+		view.setCenter(p);
 	};
 	
 	$scope.selectOffsetController = function($scope) {
@@ -113,39 +150,77 @@ teifighterController = function ($scope) {
 		var baseOffsetX = Math.floor(raster.width/2);
 		var baseOffsetY = Math.floor(raster.height/2);
 		
-		zoomIn = function(cx, cy) {
-			//this.view.zoom *= 1.5;
+		// Zooms in
+		zoomIn = function() {
 			this.layer.scale(1.5);
 			zoom *= 1.5;
 			this.view.update();
 		}
 		
+		// Zooms out without changing the position of the pixel (cx,cy)
+		// of the view
 		zoomOut = function() {
-			//this.view.zoom /= 1.5;
 			this.layer.scale(1/1.5);
 			zoom /= 1.5;
 			this.view.update();
 		}
 		
+		// Transform coordinates on the view into coordinates
+		// on the document image
 		getRealPoint = function(p) {
-			q = new paper.Point(
+			var q = new paper.Point(
 				(p.x + baseOffsetX*zoom - this.layer.position.x) / zoom,
 				(p.y + baseOffsetY*zoom - this.layer.position.y) / zoom
 			);
 			return q;
 		};
 		
-		getLayerPoint = function(p) {
-			
+		// Transforms coordinates from the document image into
+		// coordinates on the view
+		getViewPoint = function(q) {
+			var p = new paper.Point(
+				q.x*zoom + this.layer.position.x - baseOffsetX*zoom,
+				q.y*zoom + this.layer.position.y - baseOffsetY*zoom
+			);
+			return p;
 		};
 		
+		// Centers the view on the given real coordinates
+		setCenter = function(p) {
+			var q = getViewPoint(p);
+			var dw = paper.view.bounds.width  / 2;
+			var dh = paper.view.bounds.height / 2;
+			
+			var dx = dw-q.x;
+			var dy = dh-q.y;
+			offsetView(dx,dy);
+		};
+		
+		// Returns the real coordinates of the central pixel of the view
+		getCenter = function() {
+			var p = getRealPoint(new paper.Point(
+				paper.view.bounds.width/2,
+				paper.view.bounds.height/2
+			));
+			return p;
+		}
+		
+		// Offset the view by the given offset in view coordinates
 		offsetView = function(dx, dy) {
 			var z = this.view.zoom;
-			var p = new paper.Point(dx/z, dy/z);
-			//this.view.scrollBy(p);
-			this.layer.position.x += p.x;
-			this.layer.position.y += p.y;
+			this.layer.position.x += dx/z;
+			this.layer.position.y += dy/z;
 			this.view.update();
+		}
+		
+		// Offsets the view so that the point reapP is at the
+		// position screenP on the screen.
+		placePointAt = function(realP, viewP) {
+			var q  = getViewPoint(realP);
+			var dx = viewP.x - q.x;
+			var dy = viewP.y - q.y;
+			console.log(dx+" "+dy);
+			offsetView(dx, dy);
 		}
 
 		return this;
