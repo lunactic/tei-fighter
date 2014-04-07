209a769020947765e40e45658bf3f225a5ff8109
Joan Pastor <joapaspe@gmail.com>
2014-04-07 14:47:46 +0200

    Model of the current page

---
+++ src/models/pageInfo.js
@@ -0,0 +1,11 @@
+
+PageInfo = function (width, height) {
+
+    this.width  = width;
+    this.height = height;
+
+    this.title = "New Page";
+    this.idSurface = "surface1";
+
+    return this;
+}
